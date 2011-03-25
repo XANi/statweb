91def45fef325c8b4e94448ae7390a7ee5b8374e
Mariusz Gronczewski <xani666@gmail.com>
2011-03-25 23:55:59 +0100

    works now

--- startapp.sh
+++ startapp.sh
@@ -3,5 +3,5 @@ kill $(cat /tmp/statweb.pid)
 export >/tmp/exports
 DANCER_APPDIR=$PWD
 echo Starting app:
-bin/app.pl --port=3001
-#screen -fn -dmS statweb bin/app.pl --port=3001
+#bin/app.pl --port=3001
+screen -fn -dmS statweb bin/app.pl --port=3001
