58d4c56dcab0b13319272834bf8cf31e39afe38a
Mariusz Gronczewski <xani666@gmail.com>
2011-03-26 00:01:43 +0100

    spam less

--- startapp.sh
+++ startapp.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-kill $(cat /tmp/statweb.pid)
-export >/tmp/exports
+kill $(cat /tmp/statweb.pid) 2>/dev/null
 DANCER_APPDIR=$PWD
-echo Starting app:
-#bin/app.pl --port=3001
 screen -fn -dmS statweb script -a -c "bin/app.pl --port=3001" /tmp/statweb.log
