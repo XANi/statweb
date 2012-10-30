05e398d9a5974ae56c395049c6e74c2430fc66ee
XANi <xani666@gmail.com>
2012-10-31 00:47:55 +0100

    add puppet template

---
+++ bootstrap.pp
@@ -0,0 +1,15 @@
+class statweb::agent {
+    package { [
+               libcommon-sense-perl,
+               libzeromq-perl,
+               libfile-slurp-perl,
+               libyaml-perl,
+               liblog-dispatch-perl,
+               libev-perl,
+               libanyevent-perl,
+               ]:
+                   ensure => installed
+    }
+}
+
+include statweb::agent
